cfc8c4227a1132aa35f8020b3de438387a89a256
Priyadharsan T <priyadharsant4@gmail.com>
2026-09-08 12:46:06 +0530

    Frontend Init

---
+++ Frontend/electron/main.cjs
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.cjs"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (app.isPackaged) {
+    mainWindow.loadFile(
+      path.join(__dirname, "../dist/index.html")
+    );
+  } else {
+    mainWindow.loadURL("http://localhost:5173");
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
